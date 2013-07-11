5f1e97fef993e42a534401f73478a8aedebe780b
No <mail@you.com>
2013-07-11 19:26:45 +0300

    loads index.html

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
+var fs = require('fs');
 var express = require('express');
+
+var ifile = 'index.html';
+
+if (!fs.existsSync(ifile)) {
+	console.log('%s does not exist. Exiting.', ifile)
+	process.exit(1);
+}
+var index = fs.readFileSync(ifile);
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
